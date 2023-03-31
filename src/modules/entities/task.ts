77451e1cbe7d225ce45c3e204500930c06babd0c
Paulo Andreola <pauloandreola@gmail.com>
2023-03-31 13:38:34 -0300

    feat: create user and task entities

---
+++ src/modules/entities/task.ts
@@ -0,0 +1,8 @@
+export class Task {
+  'idTask': string
+  'title': string
+  'description': string
+  'effort': number
+  'createdAt': Date
+  'updateAt': Date
+}
---
+++ src/modules/entities/user.ts
@@ -0,0 +1,8 @@
+export class User {
+  'idUser': string
+  'name': string
+  'email': string
+  'password': string
+  'createdAt': Date
+  'updateAt': Date
+}

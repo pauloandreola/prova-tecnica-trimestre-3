cac1ccf0baf3df529bdd0f5a72190559745a3f52
Paulo Andreola <pauloandreola@gmail.com>
2023-04-01 08:27:39 -0300

    feat: create folder and files for task and user routes

--- src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { routerUser } from './user'
-import { routerTask } from './task'
-
-export const router = Router()
-
-router.use('/task', routerTask)
-router.use('/user', routerUser)
---
+++ src/shared/routes/index.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express'
+import { routerUser } from './user.routes'
+import { routerTask } from './task.routes'
+
+export const router = Router()
+
+// router.get('/', function (req, res) {
+//   res.status(200).json('Hello world')
+// })
+
+router.use('/task', routerTask)
+router.use('/user', routerUser)

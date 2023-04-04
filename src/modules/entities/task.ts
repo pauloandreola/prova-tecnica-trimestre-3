ab371d31f34f8be9e1c788225a7b3b0f5acc1a2c
Paulo Andreola <pauloandreola@gmail.com>
2023-04-04 17:16:39 -0300

    refactor: change format task entity, using mongoose lib

--- src/modules/entities/task.ts
+++ src/modules/entities/task.ts
@@ -1,17 +1,21 @@
-export class Task {
-  'idTask': string
-  'title': string
-  'description': string
-  'done': boolean
-  'createdAt': string
-  'updateAt': string
+import mongoose from 'mongoose'
 
-  constructor (idTask: string, title: string, description: string, done: boolean, createdAt: string, updatedAt: string) {
-    this.idTask = idTask
-    this.title = title
-    this.description = description
-    this.done = done
-    this.createdAt = createdAt
-    this.updateAt = updatedAt
-  }
+export interface ITask extends mongoose.Document {
+  _id: string;
+  title: string;
+  description?: string;
+  done: boolean;
+  created_at: Date;
+  updated_at: Date;
 }
+
+const userSchema = new mongoose.Schema({
+  _id: { type: String, required: false },
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  done: { type: Boolean, required: false }
+},
+{ timestamps: true }
+)
+
+export const UserModel = mongoose.model<ITask>('Task', userSchema)

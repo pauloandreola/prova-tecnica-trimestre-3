6df42b45f7ca89219c412c5b1b6ba7dfe49b13c2
Paulo Andreola <pauloandreola@gmail.com>
2023-04-04 17:19:22 -0300

    feat: update id format mongodb default and included hash method

--- src/modules/entities/user.ts
+++ src/modules/entities/user.ts
@@ -1,17 +1,29 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
 
 export interface IUser extends mongoose.Document {
-  id: string;
+  _id: string;
   name: string;
   email: string;
   password: string;
 }
 
 const userSchema = new mongoose.Schema({
-  id: { type: String, required: false },
+  _id: { type: String, required: false },
   name: { type: String, required: true },
   email: { type: String, required: true, lowercase: true, unique: true },
   password: { type: String, required: true }
+})
+
+userSchema.pre<IUser>('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next()
+  }
+
+  const salt = await bcrypt.genSalt(8)
+  const hash = await bcrypt.hash(this.password, salt)
+  this.password = hash
+  next()
 })
 
 export const UserModel = mongoose.model<IUser>('User', userSchema)

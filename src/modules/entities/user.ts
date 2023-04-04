bc4aa1dbb540b7e083052192f787a4f0cb15321f
Paulo Andreola <pauloandreola@gmail.com>
2023-04-04 14:07:39 -0300

    refactor: change format user entities, using mongoose lib

--- src/modules/entities/user.ts
+++ src/modules/entities/user.ts
@@ -1,13 +1,17 @@
-export class User {
-  'idUser': string
-  'name': string
-  'email': string
-  'password': string
+import mongoose from 'mongoose'
 
-  constructor (idUser: string, name:string, email: string, password: string) {
-    this.idUser = idUser
-    this.name = name
-    this.email = email
-    this.password = password
-  }
+export interface IUser extends mongoose.Document {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
 }
+
+const userSchema = new mongoose.Schema({
+  id: { type: String, required: false },
+  name: { type: String, required: true },
+  email: { type: String, required: true, lowercase: true, unique: true },
+  password: { type: String, required: true }
+})
+
+export const UserModel = mongoose.model<IUser>('User', userSchema)

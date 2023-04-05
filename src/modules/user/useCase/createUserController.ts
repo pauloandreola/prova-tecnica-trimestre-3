98345ce82046587f2b8e005ff8986fa6c59123e5
Paulo Andreola <pauloandreola@gmail.com>
2023-04-05 02:01:35 -0300

    feat: create user controller done

---
+++ src/modules/user/useCase/createUserController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express'
+
+import { UserModel, IUser } from '../../entities/user'
+
+export const createUser = async (req: Request, res: Response) => {
+  const { name, email, password } = req.body
+  try {
+    const user: IUser = new UserModel({ name, email, password })
+    await user.save()
+    res.status(201).json(user)
+  } catch (err) {
+    res.status(400).json('User already exist')
+  }
+}

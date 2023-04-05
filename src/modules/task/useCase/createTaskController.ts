06c3e107b1c241fcf37f1171e7c68d9973d1bf76
Paulo Andreola <pauloandreola@gmail.com>
2023-04-05 01:44:38 -0300

    feat: create task controller done

---
+++ src/modules/task/useCase/createTaskController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express'
+
+import { TaskModel, ITask } from '../../entities/task'
+// Método para criar uma tarefa passando os dados de título, descrição e se a tarefa está feita ou não.
+export const createTask = async (req: Request, res: Response) => {
+  const { title, description, done } = req.body
+  try {
+    const task: ITask = new TaskModel({ title, description, done })
+    await task.save()
+    res.status(201).json(task)
+  } catch (err) {
+    res.status(400).json('Task not created')
+  }
+}
---
+++ src/modules/task/useCase/deleteTaskController.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express'
+import { ObjectId } from 'mongodb'
+
+import { TaskModel, ITask } from '../../entities/task'
+// Método para deletar uma tarefa, fazendo a busca pelo ID
+export const deleteTask = async (req: Request, res: Response) => {
+  const id = req.params.id
+  try {
+    const test = ObjectId.isValid(id)
+    // Verificando se o ObjectID é válido
+    if (!test) {
+      return res.status(400).json('Invalid ID!')
+    }
+    const findTask = await TaskModel.findById(id)
+
+    if (!findTask) {
+      return res.status(404).json('Task not found!')
+    }
+
+    const task: ITask = await TaskModel.findByIdAndDelete(id)
+    res.status(200).json({ task, msg: 'Task deleted' })
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ error: err.message })
+  }
+}

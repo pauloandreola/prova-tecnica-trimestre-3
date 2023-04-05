d3d5d20b0b97073d972f08565205a2d0ad28d147
Paulo Andreola <pauloandreola@gmail.com>
2023-04-05 01:37:20 -0300

    refactor: optimized user and task entities

--- src/modules/entities/task.ts
+++ src/modules/entities/task.ts
@@ -1,21 +1,15 @@
 import { Document, Schema, model } from 'mongoose'
 
 export interface ITask extends Document {
-  _id: string;
   title: string;
   description?: string;
   done: boolean;
-  created_at: Date;
-  updated_at: Date;
 }
 
 const taskSchema = new Schema({
-  _id: { type: String },
   title: { type: String, required: true },
   description: { type: String, required: true },
-  done: { type: Boolean, default: false },
-  created_at: { type: Date, default: Date.now },
-  updated_at: { type: Date, default: Date.now }
+  done: { type: Boolean, default: false }
 
 },
 { timestamps: true }
--- src/modules/entities/user.ts
+++ src/modules/entities/user.ts
@@ -2,14 +2,12 @@ import { Document, Schema, model } from 'mongoose'
 import bcrypt from 'bcryptjs'
 
 export interface IUser extends Document {
-  _id: string;
   name: string;
   email: string;
   password: string;
 }
 
 const userSchema = new Schema({
-  _id: { type: String, required: false },
   name: { type: String, required: true },
   email: { type: String, required: true, lowercase: true, unique: true },
   password: { type: String, required: true }

19611186b84298917397ba83dba1ed5e831d417c
Paulo Andreola <pauloandreola@gmail.com>
2023-04-06 09:02:53 -0300

    refactor: optimized token verify method and update http status

--- src/shared/middlewares/authToken.ts
+++ src/shared/middlewares/authToken.ts
@@ -7,29 +7,29 @@ import { UserModel } from '../../modules/entities/user'
 
 dotenv.config()
 
-const secret = process.env.JWT
+const secret = process.env.TOKEN
+const newSecret = process.env.REFRESHTOKEN
 
-interface IToken {
-  data: string;
-}
-
-export function authToken (req: Request, res: Response, next: NextFunction) {
-  const headerAuth = req.headers.authorization
-  const token = headerAuth?.split(' ')[1]
+export async function authToken (req: Request, res: Response, next: NextFunction) {
+  const authHeader = req.headers.authorization
+  const token = authHeader?.split(' ')[1]
   if (!token) {
     return res.status(404).json('Token not found!')
   }
 
-  let data: IToken
   try {
-    data = verify(token, secret) as IToken
+    const data = verify(token, secret) as { email: string}
+    if (!secret) {
+      return res.status(401).json('Without secret string!')
+    }
+
+    const user = UserModel.findOne({ email: data.email })
+    if (!user) {
+      return res.status(401).json('Invalid user!')
+    }
   } catch (error) {
-    return res.status(422).json('Invalid token')
+    return res.status(401).json('Invalid token')
   }
 
-  const user = UserModel.findOne({ data })
-  if (!user) {
-    return res.status(422).json('Invalid user!')
-  }
   return next()
 }

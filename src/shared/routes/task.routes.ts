6c354351913d765f055f452f8e3c9381419e9d1d
Paulo Andreola <pauloandreola@gmail.com>
2023-04-06 09:08:05 -0300

    feat: create private routes, insert authentication method

--- src/shared/routes/task.routes.ts
+++ src/shared/routes/task.routes.ts
@@ -7,15 +7,17 @@ import { getTask } from '../../modules/task/useCase/getTaskController'
 import { getTaskFilter } from '../../modules/task/useCase/getTaskFilterController'
 import { updateTask } from '../../modules/task/useCase/updateTaskController'
 
+import { authToken } from '../middlewares/authToken'
+
 export const routerTask = Router()
 
 routerTask.get('/test', function (req, res) {
   res.status(200).json('Hello world TASK')
 })
 
-routerTask.post('/', createTask)
-routerTask.delete('/:id', deleteTask)
-routerTask.get('/tasks', getAllTask)
-routerTask.get('/:id', getTask)
-routerTask.get('/filter', getTaskFilter)
-routerTask.put('/:id', updateTask)
+routerTask.post('/', authToken, createTask)
+routerTask.delete('/:id', authToken, deleteTask)
+routerTask.get('/tasks', authToken, getAllTask)
+routerTask.get('/:id', authToken, getTask)
+routerTask.get('/filter/:id', authToken, getTaskFilter)
+routerTask.put('/:id', authToken, updateTask)

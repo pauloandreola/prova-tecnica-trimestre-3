9f7300d86a035197ef120a8a485010a647898ee8
Paulo Andreola <pauloandreola@gmail.com>
2023-04-08 09:17:27 -0300

    refactor: remove .env and validation not necessary

--- src/shared/middlewares/authToken.ts
+++ src/shared/middlewares/authToken.ts
@@ -7,7 +7,6 @@ import { UserModel } from '../../modules/entities/user'
 dotenv.config()
 
 const secret = process.env.TOKEN
-// const newSecret = process.env.REFRESHTOKEN
 
 export async function authToken (req: Request, res: Response, next: NextFunction) {
   const authHeader = req.headers.authorization
@@ -18,11 +17,7 @@ export async function authToken (req: Request, res: Response, next: NextFunction
 
   try {
     const data = verify(token, secret) as { email: string}
-    if (!secret) {
-      return res.status(401).json('Without secret string!')
-    }
-
-    const user = UserModel.findOne({ email: data.email })
+    const user = UserModel.findOne({ data })
     if (!user) {
       return res.status(401).json('Invalid user!')
     }

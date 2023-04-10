171e2efbdf35390bc37acf9bb60e91d00cf37a94
Paulo Andreola <pauloandreola@gmail.com>
2023-04-10 15:15:05 -0300

    feat: created filter method.

--- src/modules/task/useCase/getTaskFilterController.ts
+++ src/modules/task/useCase/getTaskFilterController.ts
@@ -1,14 +1,26 @@
 import { Request, Response } from 'express'
 
 import { TaskModel, ITask } from '../../entities/task'
-import { ConnectionClosedEvent } from 'mongodb'
+import { ObjectId } from 'mongodb'
 
 export const getTaskFilter = async (req: Request, res: Response) => {
-  const { title, done } = req.body
+  const { userId, title, done } = req.body
   try {
-    const firstFilter: ITask[] = await TaskModel.find(title)
+    // Verificando se o ObjectID é válido
+    const test = ObjectId.isValid(userId)
+    if (!test) {
+      return res.status(422).json('Invalid ID!')
+    }
 
-    res.status(200).json(firstFilter)
+    const tasksFilter: ITask[] = await TaskModel.find({
+      $and: [
+        { title: { $regex: title as string, $options: 'i' } },
+        { userId: { $regex: userId as string, $options: 'i' } },
+        { done: done as boolean }
+      ]
+    })
+
+    res.status(200).json(tasksFilter)
   } catch (err) {
     console.error(err)
     res.status(500).json({ error: err.message })

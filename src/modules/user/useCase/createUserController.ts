80bfa83ebc888274dc0799277a42c3b47c975601
Paulo Andreola <pauloandreola@gmail.com>
2023-04-09 23:06:57 -0300

    feat: add comments

--- src/modules/user/useCase/createUserController.ts
+++ src/modules/user/useCase/createUserController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express'
 
 import { UserModel, IUser } from '../../entities/user'
-
+// Método para criar um usuário com os dados de nome, email e senha
 export const createUser = async (req: Request, res: Response) => {
   const { name, email, password } = req.body
   try {

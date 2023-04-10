ad48a395519d94de702b8abce76381a324763a3b
Paulo Andreola <pauloandreola@gmail.com>
2023-04-09 23:37:57 -0300

    feat: optimized code and update comments

--- src/modules/task/useCase/updateTaskController.ts
+++ src/modules/task/useCase/updateTaskController.ts
@@ -7,19 +7,18 @@ export const updateTask = async (req: Request, res: Response) => {
   const { title, description, done } = req.body
   const id = req.params.id
   try {
-    const test = ObjectId.isValid(id)
     // Verificando se o ObjectID é válido
+    const test = ObjectId.isValid(id)
     if (!test) {
       return res.status(422).json('Invalid ID!')
     }
-    const findTask = await TaskModel.findById(id)
     // Verificando se existe o ID no banco
+    const findTask = await TaskModel.findById(id)
     if (!findTask) {
       return res.status(404).json('Task not found!')
     }
     // Buscando tarefa pelo ID e atualizando os campos da tarefa
-    const updateTask = { title, description, done }
-    const updatedTask: ITask = await TaskModel.findByIdAndUpdate(id, updateTask)
+    const updatedTask: ITask = await TaskModel.findByIdAndUpdate(id, { title, description, done })
     res.status(200).json({ updatedTask, msg: 'Task updated!' })
   } catch (err) {
     console.error(err)

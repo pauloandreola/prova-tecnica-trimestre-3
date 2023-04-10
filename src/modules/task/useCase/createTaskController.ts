aca56ae6a7a7883b7de297a9251cc01cd95bf6db
Paulo Andreola <pauloandreola@gmail.com>
2023-04-10 00:28:58 -0300

    feat: included userid property

--- src/modules/task/useCase/createTaskController.ts
+++ src/modules/task/useCase/createTaskController.ts
@@ -3,9 +3,9 @@ import { Request, Response } from 'express'
 import { TaskModel, ITask } from '../../entities/task'
 // Método para criar uma tarefa passando os dados de título, descrição e se a tarefa está feita ou não.
 export const createTask = async (req: Request, res: Response) => {
-  const { title, description, done } = req.body
+  const { userId, title, description, done } = req.body
   try {
-    const task: ITask = new TaskModel({ title, description, done })
+    const task: ITask = new TaskModel({ userId, title, description, done })
     await task.save()
     res.status(201).json(task)
   } catch (err) {

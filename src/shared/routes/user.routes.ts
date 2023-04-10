bda292ec0e20afdaa2181fbbecbd68b32bbb757c
Paulo Andreola <pauloandreola@gmail.com>
2023-04-09 21:32:43 -0300

    feat: remove getalluser endpoint, create just for test

--- src/shared/routes/user.routes.ts
+++ src/shared/routes/user.routes.ts
@@ -1,13 +1,11 @@
 import { Router } from 'express'
 
 import { createUser } from '../../modules/user/useCase/createUserController'
-import { getAllUser } from '../../modules/user/useCase/getAllUserController'
 import { loginUser } from '../../modules/user/useCase/loginUserController'
 import { refreshTokenUser } from '../../modules/user/useCase/refreshTokenUserController'
 
 export const routerUser = Router()
 
 routerUser.post('/', createUser)
-routerUser.get('/users', getAllUser)
 routerUser.post('/login', loginUser)
 routerUser.post('/refreshtoken', refreshTokenUser)
